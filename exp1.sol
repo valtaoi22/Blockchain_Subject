79af49512032c2cfbed435911b7115d53e2b717f
Vaneet Singla <91556518+valtaoi22@users.noreply.github.com>
2023-08-05 10:16:38 +0530

    Update exp1.sol

---
+++ exp1.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract exp1
+{
+    string public hello;
+    function print() public returns(string memory){
+     hello="hello vaneet!";
+     return hello;
+    }
+    
+
+}
